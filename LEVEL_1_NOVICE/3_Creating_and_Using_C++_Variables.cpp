7f56422fd083c6a832e1931e624689f18db78bfd
RS-codes <RS-codes@github.com>
2020-01-02 10:10:47 +0530

    3_Creating_and_Using_C++_Variables.cpp

---
+++ LEVEL_1_NOVICE/3_Creating_and_Using_C++_Variables.cpp
@@ -0,0 +1,17 @@
+//#3. Creating and Using C++ Variables :
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int age; // variable definition(whch type of data
+/*VARIABLE age of TYPE integer*/
+	float average=32.36; //variable initialized
+	char sex='M';
+	bool isit=true; //true=1,false=0
+	short int discountprice=10; //typemodifier
+	age=26; //can de declared anywer, after defining it 
+
+	cout<<"age= "<<age<<endl<<"average= "<<average<<endl<<"sex= "<<sex<<endl<<"isit= "<<isit<<endl<<"Offer= "<<discountprice<<endl;
+	return 0;
+}
