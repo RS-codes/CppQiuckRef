4512a949854f99ceffd57a877865ee0eedf0ce96
RS-codes <RS-codes@github.com>
2020-01-03 09:46:34 +0530

    4_Console_Output_with_Cout.cpp

---
+++ LEVEL_1_NOVICE/4_Console_Output_with_Cout.cpp
@@ -0,0 +1,10 @@
+//#4. C++ Console Output with Cout :
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	int a=10;
+	cout<<"a= "<<a<<endl;//cout(console output)->object of ostream Class, << stream insertion operator
+	return 0;
+}
